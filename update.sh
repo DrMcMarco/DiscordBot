0ba42ef3cb3aeb95d3d641770ea84e13ba9df6a5
root <root@h2867845.stratoserver.net>
2022-10-20 10:49:44 +0200

    Testing if you can just pull the rug from under the bot and then restart

--- update.sh
+++ update.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-sleep 5
-systemctl stop discord-bot
-sleep 5
 echo 'Updating...'
 git pull origin master
 echo 'Finished. Starting bot...'
-systemctl start discord-bot
+systemctl restart discord-bot
